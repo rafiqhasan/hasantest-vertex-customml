b19ad814448511ab4bef9ebeca07d01eb7453bcf
hasan <hasanrafiq@google.com>
2022-01-02 20:25:20 +0000

    hasan

--- pipelines/train_pipeline.py
+++ pipelines/train_pipeline.py
@@ -9,6 +9,100 @@ from google.cloud import aiplatform
 from google_cloud_pipeline_components import aiplatform as gcc_aip
 from typing import NamedTuple
 
+from google.cloud import aiplatform
+
+@component(
+    base_image="gcr.io/deeplearning-platform-release/tf2-cpu.2-7:latest",
+    output_component_file="tabular_eval_component.yaml",
+    packages_to_install=["google-cloud-aiplatform"],
+)
+def create_hyperparameter_tuning_job_python_package_sample(
+    project: str,
+    display_name: str,
+    service_account: str,
+    executor_image_uri: str,
+    package_uri: str,
+    python_module: str,
+    hpt_args: list,
+    location: str = "us-central1",
+    api_endpoint: str = "us-central1-aiplatform.googleapis.com"
+):
+    from google.cloud import aiplatform
+    import time
+    
+    _POLLING_INTERVAL_IN_SECONDS = 20
+
+    _JOB_COMPLETE_STATES = (
+        'JobState.JOB_STATE_SUCCEEDED',
+        'JobState.JOB_STATE_FAILED',
+        'JobState.JOB_STATE_CANCELLED',
+        'JobState.JOB_STATE_PAUSED'
+    )
+    
+    # The AI Platform services require regional API endpoints.
+    client_options = {"api_endpoint": api_endpoint}
+    # Initialize client that will be used to create and send requests.
+    # This client only needs to be created once, and can be reused for multiple requests.
+    client = aiplatform.gapic.JobServiceClient(client_options=client_options)
+
+    # study_spec
+    metric = {
+        "metric_id": "eval_rmse",
+        "goal": aiplatform.gapic.StudySpec.MetricSpec.GoalType.MINIMIZE,
+    }
+
+    lr = {
+            "parameter_id": "lr",
+            "double_value_spec": {"min_value": 0.001, "max_value": 0.05}
+    }
+    
+    hidden_layers = {
+            "parameter_id": "hidden_layers",
+            "integer_value_spec": {"min_value": 1, "max_value": 4}
+    }
+
+    # trial_job_spec
+    machine_spec = {
+        "machine_type": "n1-standard-4"
+    }
+    worker_pool_spec = {
+        "machine_spec": machine_spec,
+        "replica_count": 1,
+        "python_package_spec": {
+            "executor_image_uri": executor_image_uri,
+            "package_uris": [package_uri],
+            "python_module": python_module,
+            "args": hpt_args,
+        },
+    }
+
+    #Create HPT Job
+    hyperparameter_tuning_job = {
+        "display_name": display_name,
+        "max_trial_count": 2,
+        "parallel_trial_count": 2,
+        "study_spec": {
+            "metrics": [metric],
+            "parameters": [lr, hidden_layers],
+            "algorithm": aiplatform.gapic.StudySpec.Algorithm.RANDOM_SEARCH,
+        },
+        "trial_job_spec": {"worker_pool_specs": [worker_pool_spec], "service_account": service_account},
+    }
+    parent = f"projects/{project}/locations/{location}"
+    name = client.create_hyperparameter_tuning_job(
+        parent=parent, hyperparameter_tuning_job=hyperparameter_tuning_job
+    )
+    print("HPT Job:", name.name)
+    
+    #Get status of HPT job recursively
+    status = client.get_hyperparameter_tuning_job(name=name.name) #initial status
+    while str(status.state) not in _JOB_COMPLETE_STATES:
+        status = client.get_hyperparameter_tuning_job(name=name.name)
+        print("HPT Status:", str(status.state))
+        time.sleep(_POLLING_INTERVAL_IN_SECONDS)
+        
+    print("HPT ended")
+
 #Main pipeline class
 class pipeline_controller():
     def __init__(self, template_path, display_name, pipeline_root, project_id, region):
@@ -57,6 +151,29 @@ class pipeline_controller():
             eval_op = kfp.components.load_component('component_specs/regression_eval_model.yaml')
 
             #STEP: For non Auto-ML call
+            hpt_args = ['--train_file', 'gs://gcs-hasanrafiq-test-331814/ml_data/taxi_dataset/train.csv',
+                         '--eval_file', 'gs://gcs-hasanrafiq-test-331814/ml_data/taxi_dataset/eval.csv',
+                         '--model_save_location', 'gs://gcs-hasanrafiq-test-331814/ml_data/taxi_dataset/model/',
+                         '--hidden_layers','2',
+                         '--experiment_name', "{{$.inputs.parameters['display_name']}}-hpt-job" ,  #This is also a way to pass a parameter as input
+                         '--mlmd_region', "{{$.inputs.parameters['pipelineparam--gcp_region']}}" ,
+                         '--project', "{{$.inputs.parameters['project']}}"
+                        ]
+            
+            #HPT
+            hpt_task = create_hyperparameter_tuning_job_python_package_sample(
+                project=project,
+                display_name=display_name,
+                service_account='318948681665-compute@developer.gserviceaccount.com',  ##Needed for Vertex MLMD access
+                executor_image_uri='us-docker.pkg.dev/vertex-ai/training/tf-cpu.2-7:latest',
+                package_uri="gs://gcs-hasanrafiq-test-331814/ml_data/taxi_dataset/ml_scripts/trainer-0.1.tar.gz",
+                python_module="trainer.task",
+                hpt_args=hpt_args,
+                location=gcp_region,
+                api_endpoint=str(f"{gcp_region}-aiplatform.googleapis.com")
+            )
+
+            #Training
             training_args = ['--train_file', 'gs://gcs-hasanrafiq-test-331814/ml_data/taxi_dataset/train.csv',
                              '--eval_file', 'gs://gcs-hasanrafiq-test-331814/ml_data/taxi_dataset/eval.csv',
                              '--model_save_location', 'gs://gcs-hasanrafiq-test-331814/ml_data/taxi_dataset/model/',
@@ -66,7 +183,7 @@ class pipeline_controller():
                              '--mlmd_region', str(gcp_region) ,
                              '--project', str(project) ,
                             ]
-
+            
             training_op = gcc_aip.CustomPythonPackageTrainingJobRunOp(
                             project=project,
                             display_name=display_name,
@@ -81,6 +198,7 @@ class pipeline_controller():
                             args=training_args
                           )
 
+            #Model Evaluation
             model_eval_task = eval_op(
                                     project,
                                     gcp_region,
